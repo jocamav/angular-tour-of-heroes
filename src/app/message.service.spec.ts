c83f599ba9558ca703971f0df5d88d56088f23a7
Jose Carlos <josecarlosmartinez@gmail.com>
2019-02-16 07:43:13 +0100

    Refactor test to include injecton in beforeEach

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -3,20 +3,23 @@ import { TestBed } from '@angular/core/testing';
 import { MessageService } from './message.service';
 
 describe('MessageService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  let service: MessageService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({ providers: [MessageService] });
+    service = TestBed.get(MessageService);
+  });
 
   it('should be created', () => {
-    const service: MessageService = TestBed.get(MessageService);
     expect(service).toBeTruthy();
   });
 
   it('#getMessages() should return empty messages list', () => {
-    const service: MessageService = TestBed.get(MessageService);
     expect(service.getMessages().length).toBe(0);
   });
 
   it('#add(message: string) should insert one message', () => {
-    const service: MessageService = TestBed.get(MessageService);
     const message: string = 'Some message';
     service.add(message);
     expect(service.getMessages().length).toBe(1);
@@ -24,7 +27,6 @@ describe('MessageService', () => {
   });
 
   it('#clear() should return one message after insert one message', () => {
-    const service: MessageService = TestBed.get(MessageService);
     service.add('message #1');
     service.add('message #2');
     service.add('message #3');
