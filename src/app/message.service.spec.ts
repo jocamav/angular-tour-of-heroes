dab151fefc601c7751fefae382d63c9a4ff08b1b
Jose Carlos <josecarlosmartinez@gmail.com>
2019-02-16 07:34:54 +0100

    Include tests for message.service

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -9,4 +9,30 @@ describe('MessageService', () => {
     const service: MessageService = TestBed.get(MessageService);
     expect(service).toBeTruthy();
   });
+
+  it('#getMessages() should return empty messages list', () => {
+    const service: MessageService = TestBed.get(MessageService);
+    expect(service.getMessages().length).toBe(0);
+  });
+
+  it('#add(message: string) should insert one message', () => {
+    const service: MessageService = TestBed.get(MessageService);
+    const message: string = 'Some message';
+    service.add(message);
+    expect(service.getMessages().length).toBe(1);
+    expect(service.getMessages()[0]).toBe(message);
+  });
+
+  it('#clear() should return one message after insert one message', () => {
+    const service: MessageService = TestBed.get(MessageService);
+    service.add('message #1');
+    service.add('message #2');
+    service.add('message #3');
+    service.add('message #4');
+    expect(service.getMessages().length).toBe(4);
+
+    service.clear();
+    expect(service.getMessages().length).toBe(0);
+  });
+
 });
